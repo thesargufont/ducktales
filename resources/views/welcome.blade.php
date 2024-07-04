991aa7ac8a35564eb5506ee41b1b56ff238bf9f5
thesar.g.agrinusa <thesar.g.agrinusa@polytron.co.id>
2024-07-04 09:50:26 +0700

    omelete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    omelet, toast, bacon and you ?
+                    omelet, toast, bacon and you :)
                 </div>
 
                 <div class="links">
